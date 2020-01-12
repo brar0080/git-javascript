51ef93b6a123bd75023131d9b4e11362a11fc8a3
Ramanpreet Brar <ramanpreetbrar@Ramanpreets-MacBook-Pro.local>
2020-01-12 17:42:01 -0500

    completed all parts of step 10

--- script.js
+++ script.js
@@ -4,5 +4,11 @@ console.log(`Hello, my name is ${name}`)
 var number = 8
 
 var result1 = number * 2
+var result2 = result1 * 5
+var result3 = result2 / 8
+var result4 = result3 - 7
 
 console.log(`multiply result of number by 2=: ${result1}`)
+console.log(`multiply result of result1=: ${result2}`)
+console.log(`divide result2 by number=: ${result3}`)
+console.log(`subtract 7 from result3=: ${result4}`)
